6869227263a54dd1d02ef3445e9b50119248de1c
JorgeBCh <jorgebch1513@gmail.com>
2022-03-15 10:16:33 +0000

    First full version of cpfromAMIi and cptoAMIi. Not easy.

--- scripts_replace_string.sh
+++ scripts_replace_string.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 files=`ls aws*.sh instances*.sh`
 case $# in
-0|1) 	echo "${0##./} replaces a string for another string in all scripts to manage multiple aws instances."
-	echo "usage:   ${0##./} \"string1\" \"string\""   
+0|1) 	echo "${0##./} replaces a string for another string in all scripts that manage multiple aws instances."
+	echo "usage:   ${0##./} \"stringToReplace\" \"stringNew\""   
 	echo
 	echo "example:   ${0##./} \"-srcCSGC-AMI04\" \"-src*\""
+	echo 
+	echo "           replaces  -srcCSGC-AMI04  with  -src\*    NB: you must double quote the strings"
 	echo 
 	echo These are the files to process:
 	for file in $files
@@ -21,7 +23,7 @@ echo $files
 for file in $files
 do
     echo File: $file
-    echo "------> GREP:"				# echo doesn't like the ----
+    echo "------> GREP:"				# echo doesn't like the ---- without double quotes.
     grep --colour -i -e  "$1" $file			# grep  "\-srcCSGC-AMI04" aws_instances_launch.sh
     echo "------> SED:"
     sed --in-place=bak "s/$1/$2/" $file			# sed -n "s/-srcCSGC-AMI04/-src\*/p" aws_instances_launch.sh

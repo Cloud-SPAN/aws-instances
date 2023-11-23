e624a60f943d8ed3b164f124697a3e04b0d64de5
JorgeBCh <jorgebch1513@gmail.com>
2023-11-23 16:24:57 +0000

    Add bin-dir linking to the scripts to ease maintenance

---
+++ thescripts_binDirLinks_make.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+source colour_utils_functions.sh	 # to add colour to some messages
+
+files=`ls *.sh`
+case $# in
+0) 	message "$(colour lb $(basename $0)) creates soft links to each of the scripts (.sh file) in the 
+current directory in the specified \"bin\" directory. The current directory is:
+	$(colour cyan `pwd`)
+
+  $(colour lb usage):    $(basename $0)  \"bin_directory\"
+  $(colour lb example):  $(basename $0)  ~/.local/bincsaws   (Jorge's location - specify yours)
+
+  $(colour lb $(basename $0)) $(colour lr "must be run") in the directory where the scripts source files (to link) 
+reside. Creating such links makes it possible (1) to run the scripts from any location in the system 
+and (2) to git-manage the scripts separately from other scripts in the target bin directory.
+  The target $(colour cyan bin) directory $(colour lr "must exist") and be specified in the execution PATH. 
+
+  $(colour lb "Files to link (NB: files or links with same names in target bin directory will be deleted)"):"
+	ls *.sh
+	exit 2;;
+*)	;;	# just continue below
+esac
+
+message "===================================="
+target_bin_directory=$1
+if [ ! -d $target_bin_directory ]; then
+    message "..Target bin directory $(colour cyan $target_bin_directory) $(colour lr "does not exist"). Create it before running this script."
+    exit 2
+fi
+
+source_directory=`pwd`
+message "Creating soft links in bin directory $(colour cyan $target_bin_directory) to each .sh file in $(colour cyan $source_directory)"
+
+cd $target_bin_directory
+message "\nworking in directory $(colour cyan `pwd`):"
+
+for file in $files
+do
+    if [ -f "$file" ]; then
+	echo "removing  $file  before creating link"
+	rm  "$file"
+    else
+	echo "$file does not exist"
+    fi
+    message "creating link: ln -s $source_directory/$file $file"  
+    ln -s $source_directory/$file $file
+done
+message "FINISHED creating links"
+
+
